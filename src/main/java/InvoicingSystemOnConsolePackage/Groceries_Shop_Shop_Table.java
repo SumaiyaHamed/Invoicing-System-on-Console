787b2c135df15508affccf5c2e4e71d3aee98de6
SumaiyaHamed <sumaiyahit@gmail.com>
2023-01-30 15:29:42 +0400

    create Groceries_Shop_Shop_Table classwith create table method

---
+++ src/main/java/InvoicingSystemOnConsolePackage/Groceries_Shop_Shop_Table.java
@@ -0,0 +1,37 @@
+package InvoicingSystemOnConsolePackage;
+
+import java.sql.Driver;
+import java.sql.DriverManager;
+
+public class Groceries_Shop_Shop_Table {
+	
+	public static boolean createTable() {
+
+		String url = "jdbc:mysql://localhost:3306/Groceries_Shop";
+		String user = "root";
+		String pass = "root";
+		String sqlDB = "CREATE TABLE Shop " + "(shop_ID INTEGER," +
+		                                      "  shop_name  VARCHAR(255), " +
+				                              " PRIMARY KEY ( id ))";
+		java.sql.Connection conn = null;
+		try {
+			Driver driver = (Driver) Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+			DriverManager.registerDriver(driver);
+			conn = DriverManager.getConnection(url, user, pass);
+			java.sql.Statement st = conn.createStatement();
+			int m = st.executeUpdate(sqlDB);
+			if (m >= 1) {
+				System.out.println("Created table in given database...");
+				return true;
+			} else {
+				System.out.println(" table already Created in given database...");
+			}
+			conn.close();
+		} catch (Exception ex) {
+			System.err.println(ex);
+		}
+		return false;
+
+	}
+
+}
